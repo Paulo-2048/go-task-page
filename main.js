6a7d6a0f9668d630282f083a933772cbebafc4a3
Paulo Vitor <paulo19032004@gmail.com>
2022-11-02 14:38:57 -0300

    Correção de caminhos

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ try {
     throw "Não Autorizado";
   }
 } catch (error) {
-  if (location.pathname != "/login.html")
-    window.location.replace("/login.html");
+  if (location.pathname != "./login.html")
+    window.location.replace("./login.html");
 }
